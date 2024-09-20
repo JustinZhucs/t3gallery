112994ec28c1c7aec2da75d9596012130138f337
Justin Zhu <justinzhu0913@gmail.com>
2024-09-20 01:11:43 -0400

    Save current changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,16 +15,8 @@ const mockImages = mockUrls.map((url, index) => ({
 
 export default function HomePage() {
   return (
-    <main className="">
-      {/* <div className="flex flex-wrap gap-4"> */}
-      <div>
-        hello
-        {/* {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} />
-          </div>
-        ))} */}
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Hello (gallery in progress)
     </main>
   );
 }
