7c1d807b238329a5017cff7888f0fa0e2980690d
Justin Zhu <justinzhu0913@gmail.com>
2024-09-20 01:19:55 -0400

    Revert "delete contents in README.md"
    This reverts commit 71a4933e239eeeb45ee96b75d0702091ef0cbbdf.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,9 @@
 import Link from "next/link";
 
-
-const mockUrls = [
-  "https://utfs.io/f/Jc1XPCrKHbsUpvXUhFIpI2ZXeRULCxnB3zDyHb489w5rN0ai",
-  "https://utfs.io/f/Jc1XPCrKHbsURysldQ2ePOaCqLAumT8vYEsg72UXV9yBRMWi",
-  "https://utfs.io/f/Jc1XPCrKHbsUTs2CNmdYFlHUEZKIAT3rOvaVhCtLf7RMgz4J",
-  "https://utfs.io/f/Jc1XPCrKHbsUO5exwSJB3yHLVENS4hnstRg7Z0dUFCjomuwf",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+      Hello (gallery in progress)
     </main>
   );
 }
